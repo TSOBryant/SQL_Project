a0231d1153633bf226045df6d04afa42526c9240
Samuel O'Bryant <104179260+TSOBryant@users.noreply.github.com>
2023-08-17 18:13:32 -0500

    Add files via upload

---
+++ Covid SQL Code v2.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[running_vaccination_count]
+  FROM [CovidProject].[dbo].[PercentPopulationVaccinated]
